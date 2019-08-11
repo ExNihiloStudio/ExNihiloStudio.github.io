32ebbca178dcac4cbaa1ceeea798aab203605bcc
ExNihiloStudio <52670998+ExNihiloStudio@users.noreply.github.com>
2019-08-11 09:41:05 -0600

    Update Furtasma.js

--- Games/Furtasma.js
+++ Games/Furtasma.js
@@ -4,5 +4,7 @@ console.log("Furtasma Javascript loaded...");
 
 function MAIN()
 {
+  if(confirm("This game contains strong sexual themes, yes, even with the character creator. By clicking on "Ok", you acknowledge that you are at or over the age of 18. You have been warned.
+  
   document.getElementById("MainText").innerHTML = "testerooniedoonie";
 }
